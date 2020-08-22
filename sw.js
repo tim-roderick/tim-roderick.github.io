6527c2a9dc78787ffde6c08cf9421fa173612c0e
root <root@DESKTOP-G5SCSCL.localdomain>
2020-08-22 14:21:21 +0100

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-1a9426a383a454d10bb2.js"
+    "url": "webpack-runtime-351c3bfb2d85ca81139b.js"
   },
   {
     "url": "styles.b91bb86593fdebf59453.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "3087662442c1ce772e83f91d1391ad81"
+    "revision": "ead92ae1efce5206596ed3ef752a99d4"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-c9875e2473a7c518755f.js"
